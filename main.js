ac5b472380447438d491bcbaf20ec8e6558418f2
Artem Aleksandruk <artem.alek@icloud.com>
2017-01-30 18:04:32 -0500

    basic css for calculator and intial work on .js

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var box1 = document.getElementById("button1");
+var box2 = document.getElementById("button2");
+var box3 = document.getElementById("button3");
+var box4 = document.getElementById("button4");
+var box5 = document.getElementById("button5");
+var box6 = document.getElementById("button6");
+var box7 = document.getElementById("button7");
+var box8 = document.getElementById("button8");
+var box9 = document.getElementById("button9");
+var box0 = document.getElementById("button0");
+var boxdot = document.getElementById("button.");
+var boxequal = document.getElementById("buttonequal");
+var boxplus = document.getElementById("buttonplus");
+var boxsub = document.getElementById("buttonsub");
+var boxdivide = document.getElementById("buttondivide");
+var boxmult = document.getElementById("buttonx");
